a13605326a88ee0191582793a7c9e94eede7009e
Maratah <maratah7@gmail.com>
2020-08-02 17:09:38 +0300

    add the classes on models. -register my models. -include gaalleria as the admin model>

--- galleria/admin.py
+++ galleria/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Location, Gallery, images
+
+class GalleryAdmin(admin.ModelAdmin):
+    filter_horizontal =('images',)
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Gallery)
+admin.site.register(images)
