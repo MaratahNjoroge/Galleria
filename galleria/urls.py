6d48a970dc1d99b12c950ae3509b638f4d93965f
Maratah <maratah7@gmail.com>
2020-08-05 08:19:44 +0300

    modify file. -add input to the file.

--- galleria/urls.py
+++ galleria/urls.py
@@ -1,9 +1,15 @@
 from django.conf.urls import url
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     url('^$',views.home,name='home'),
-    url('^gallery/$',views.gallery,name='gallery'),
+    url(r'^about',views.about,name='about'),
     url(r'^image/(\d+)', views.image, name='image'),
-    url('^gallery/$',views.gallery,name='gallery')
+    url(r'^gallery/$',views.gallery,name='gallery'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
